7b49bd14095c81bc9f52b48e7f315ea3c0252768
Remo Kessler <dev@remok.dev>
2024-09-23 14:16:40 +0200

    add wdio

--- libs/ui-core-wc-atoms/src/lib/lit-button.component.spec.ts
+++ libs/ui-core-wc-atoms/src/lib/lit-button.component.spec.ts
@@ -1,11 +1,13 @@
-import { BuiLitButtonComponent } from './lit-button.component';
+import { html, render } from 'lit';
+import { $, expect } from '@wdio/globals';
+
+import './lit-button.component';
 
 describe('BuiLitButtonComponent', () => {
-  let button: BuiLitButtonComponent;
-  beforeEach(() => {
-    button = new BuiLitButtonComponent();
-  });
-  it('should be defined', () => {
-    expect(button).toBeDefined();
+  it('should be defined', async () => {
+    render(html` <bui-button></bui-button>`, document.body);
+
+    const button = await $('bui-button').$('button');
+    await expect(button).toHaveText("I'm a Lit-Button!");
   });
 });

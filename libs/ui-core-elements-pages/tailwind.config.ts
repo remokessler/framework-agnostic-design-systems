86ce38195d676c127ac4b6c7131aab6cf8286ec6
Remo Kessler <dev@remok.dev>
2025-01-17 16:07:55 +0100

    fix linters

--- libs/ui-core-elements-pages/tailwind.config.ts
+++ libs/ui-core-elements-pages/tailwind.config.ts
@@ -1,4 +1,3 @@
-import { createGlobPatternsForDependencies } from '@nx/angular/tailwind';
 import { join } from 'path';
 // eslint-disable-next-line
 import { TailwindConfig } from '../../libs/ui-core-styles/src/tailwind.config';
@@ -6,7 +5,7 @@ import { TailwindConfig } from '../../libs/ui-core-styles/src/tailwind.config';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [TailwindConfig],
-  content: [join(__dirname, '../**/!(*.spec).{ts,html}'), ...createGlobPatternsForDependencies(__dirname)],
+  content: [join(__dirname, '../**/!(*.spec).{ts,html}')],
   theme: {
     extend: {},
   },
--- libs/ui-core-styles/jest.config.ts
+++ libs/ui-core-styles/jest.config.ts
@@ -2,21 +2,10 @@
 export default {
   displayName: 'ui-core-styles',
   preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/libs/ui-core-styles',
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
-      },
-    ],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/ui-core-styles',
 };
